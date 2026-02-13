4bf6352e165fc36ba3a7a6478de4011787260c90
Thanh Huy <vothanhhuy7102004@gmail.com>
2026-02-13 22:55:13 +0700

    first commit

---
+++ src/main/java/com/example/HMovie/HMovieApplication.java
@@ -0,0 +1,13 @@
+package com.example.HMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HMovieApplication.class, args);
+	}
+
+}
